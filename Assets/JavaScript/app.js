9fbb38217ff0be43baa78a5360647647ddfcbd98
antoine bartholomew <antoinebartholomew@antoines-MacBook-Pro.local>
2017-12-22 01:33:49 -0500

    psudeo code part 1

--- Assets/JavaScript/app.js
+++ Assets/JavaScript/app.js
@@ -1,23 +1,46 @@
-  var animals = [];
+//create an array of strings,
 
+var corvett = [];
 
+var mustang = [];
 
-  $("button").on("click", function() {
-    // Grabbing and storing the data-animal property value from the button
-    var animal = $(this).attr("data-animal");
+var charger = [];
 
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+var porsche = [];
 
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      // After data comes back from the request
-      .done(function(response) {
-        console.log(queryURL);
+var topics;
 
-        console.log(response);
-      });
+//Try using a loop that appends a button for each string in the array
+
+//When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
+//When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+            // <img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" data-state="still" class="gif">
+            // <img src="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif" data-still="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif" data-animate="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200.gif" data-state="still" class="gif">
+            // <img src="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" data-still="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" data-animate="https://media3.giphy.com/media/W6LbnBigDe4ZG/200.gif" data-state="still" class="gif">
+            // <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            // <script type="text/javascript">
+            //
+            //   $(".gif").on("click", function() {
+            //     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            //     var state = $(this).attr("data-state");
+            //     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            //     // Then, set the image's data-state to animate
+            //     // Else set src to the data-still value
+            //     if (state === "still") {
+            //       $(this).attr("src", $(this).attr("data-animate"));
+            //       $(this).attr("data-state", "animate");
+            //     } else {
+            //       $(this).attr("src", $(this).attr("data-still"));
+            //       $(this).attr("data-state", "still");
+            //     }
+            //   });
+
+//Under every gif, display its rating (PG, G, so on).
+
+//Only once you get images displaying with button presses should you move on to the next step.
+
+//Add a form to your page takes the value from a user input box and adds it into your topics array.
+
+//Then make a function call that takes each topic in the array remakes the buttons on the page.
